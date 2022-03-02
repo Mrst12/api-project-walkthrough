4175de44ab2f1610965acb1a6cbabe67da2995ba
Lisa Tinmurth <LisaTinmurth@hotmail.com>
2022-03-02 11:42:15 +0000

    Add first part of the post request

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,20 @@ const API_KEY = "dg_dQ-0THOt8NhKtZ-qoXjudUAo";
 const API_URL = "https://ci-jshint.herokuapp.com/api";
 const resultsModal = new bootstrap.Modal(document.getElementById("resultsModal"));
 
-document. getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("status").addEventListener("click", e => getStatus(e));
+document.getElementById("submit").addEventListener("click", e => postForm(e));
+
+async function postForm(e) {
+    const form = new FormData(document.getElementById("checksform"));
+
+    const response = await fetch(API_URL, {
+        method: "POST",
+        headers: {
+            "Authorization": API_KEY,
+        },
+        body: form,
+    });
+}
 
 async function getStatus(e) {
     const queryString = `${API_URL}?api_key=${API_KEY}`;
